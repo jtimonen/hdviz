05a4df929cee58c24a0b8e79ed22ecb2e8258738
jtimonen <juho.timonen@iki.fi>
2021-04-29 01:23:47 +0300

    edit setup

--- hdviz/__init__.py
+++ hdviz/__init__.py
@@ -3,12 +3,7 @@ from .plotter_3d import Plotter3d
 from .plotter_nd import PlotterNd
 from .utils import create_grid_around
 
-try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:
-    import importlib_metadata
-
-package_name = "hdviz"
-__version__ = importlib_metadata.version(package_name)
+# Version defined here
+__version__ = "0.0.3"
 
 __all__ = ["Plotter2d", "Plotter3d", "PlotterNd", "create_grid_around"]
--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
+import os
+import re
 import setuptools
 
+pkg_name = "hdviz"
+
+# Get long description
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
@@ -9,9 +14,18 @@ with open("README.md", "r", encoding="utf-8") as fh:
 with open("requirements.txt", "r") as fh:
     install_requires = fh.read()
 
+# Get version from hdviz/__init__.py
+here = os.path.realpath(os.path.dirname(__file__))
+with open(os.path.join(here, pkg_name, "__init__.py")) as f:
+    meta_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", f.read(), re.M)
+    if meta_match:
+        version = meta_match.group(1)
+    else:
+        raise RuntimeError("Unable to find __version__ string.")
+
 setuptools.setup(
-    name="hdviz",
-    version="0.0.3",
+    name=pkg_name,
+    version=version,
     author="Juho Timonen",
     author_email="juho.timonen@iki.fi",
     description="High-dimensional visualization.",
